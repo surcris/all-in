07e0d8a221208a6e4adc57814df5a568256ca20e
Niel Abdallah <sympta113@gmail.com>
2024-05-28 05:27:36 +0200

    route et merge des features home et homecult

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { RouterModule, Routes, } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
+import { HomeCultFormComponent } from './CultForm/home-cult-form/home-cult-form.component';
 
 
 
 export const routes: Routes = [
     { path: '', title: "accueil", component: HomeComponent },
+    { path: 'HomeCult', component: HomeCultFormComponent },
 ];
 
 
