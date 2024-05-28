023d952be7becc2ba411c4ac1a7c0edfac625274
Niel Abdallah <sympta113@gmail.com>
2024-05-28 05:18:12 +0200

    home et header

--- src/app/CultForm/home-cult-form/home-cult-form.component.ts
+++ src/app/CultForm/home-cult-form/home-cult-form.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderCultFormComponent } from '../header-cult-form/header-cult-form.component';
 
 @Component({
   selector: 'app-home-cult-form',
   standalone: true,
-  imports: [],
+  imports: [HeaderCultFormComponent],
   templateUrl: './home-cult-form.component.html',
   styleUrl: './home-cult-form.component.scss'
 })
