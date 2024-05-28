ed595a7c5e583e500b88a3ff9e074fa7cff4a99f
Niel Abdallah <sympta113@gmail.com>
2024-05-28 06:02:26 +0200

    formulaire inscr et connex

--- src/app/CultForm/header-cult-form/header-cult-form.component.ts
+++ src/app/CultForm/header-cult-form/header-cult-form.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-header-cult-form',
   standalone: true,
-  imports: [],
+  imports: [ RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './header-cult-form.component.html',
   styleUrl: './header-cult-form.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,17 @@ import { RouterModule, Routes, } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { HomeCultFormComponent } from './CultForm/home-cult-form/home-cult-form.component';
+import { InscriptionComponent } from './CultForm/inscription/inscription.component';
+import { ConnexionComponent } from './CultForm/connexion/connexion.component';
+
 
 
 
 export const routes: Routes = [
     { path: '', title: "accueil", component: HomeComponent },
     { path: 'HomeCult', component: HomeCultFormComponent },
+    { path: 'inscription',title:"inscription" , component: InscriptionComponent },
+    { path: 'connexion',title:"connexion" , component: ConnexionComponent },
 ];
 
 
