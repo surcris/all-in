3201619091f7b3051ce9dcad70c8385592a81612
Niel Abdallah <sympta113@gmail.com>
2024-06-01 19:16:13 +0200

    auth co et ins

--- src/app/CultForm/connexion/connexion.component.ts
+++ src/app/CultForm/connexion/connexion.component.ts
@@ -2,28 +2,68 @@ import { Component, OnInit  } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators  } from '@angular/forms';
 import { HeaderCultFormComponent } from '../header-cult-form/header-cult-form.component';
 import { NgIf } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+
+import * as CryptoJS from 'crypto-js'
+import { environment } from '../../../environments/environment';
+
 @Component({
   selector: 'app-connexion',
   standalone: true,
-  imports: [HeaderCultFormComponent],
+  imports: [ReactiveFormsModule, HeaderCultFormComponent, NgIf],
   templateUrl: './connexion.component.html',
   styleUrl: './connexion.component.scss'
 })
 export class ConnexionComponent implements OnInit {
-
+ 
   authCoForm: FormGroup;
-
-  constructor(private formBuilder: FormBuilder){
+  messageError:String;
+  formSubmitted: boolean = false;
+  constructor(private formBuilder: FormBuilder,private http: HttpClient){
     this.authCoForm = this.formBuilder.group({
-      email: ['' , Validators.required],
+      email: ['' , [Validators.required, Validators.email]],
       password: ['' , Validators.required]
     })
+
+    this.messageError = '';
   }
   ngOnInit() {
     
   }
-
+  m:string = "";
+  test(){
+    console.log(environment.akey)
+  }
+  decrypt(ciphertext:string,key:string) {
+    const bytes = CryptoJS.AES.decrypt(ciphertext, key);
+    return bytes.toString(CryptoJS.enc.Utf8);
+  }
   onSubmit(){
+
+    this.formSubmitted = true;
     
+    if(!this.authCoForm.invalid){
+     
+      const formData = {
+        email: this.authCoForm.value.email,
+        password: this.authCoForm.value.password
+      };
+      
+      // console.log(CryptoJS.AES.encrypt(this.authCoForm.value.password,environment.akey).toString() )
+      // console.log(formData)
+      this.http.put(environment.apiUrl+"/auth/connexionUser", formData).subscribe(
+        (response: any) => {
+          // console.log('Form submitted successfully', response);
+          
+          // this.m = response.message;
+          // localStorage.setItem('akey', response.message);
+          
+        },
+        error => {
+          console.error('Error submitting form', error);
+        }
+      );
+      
+    }
   }
 }
--- src/app/CultForm/inscription/inscription.component.ts
+++ src/app/CultForm/inscription/inscription.component.ts
@@ -1,16 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HeaderCultFormComponent } from '../header-cult-form/header-cult-form.component';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators  } from '@angular/forms';
+import { NgIf } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-inscription',
   standalone: true,
-  imports: [HeaderCultFormComponent],
+  imports: [ReactiveFormsModule, HeaderCultFormComponent, NgIf],
   templateUrl: './inscription.component.html',
   styleUrl: './inscription.component.scss'
 })
-export class InscriptionComponent {
+export class InscriptionComponent implements OnInit{
 
+  authInsForm:FormGroup;
+  formSubmitted: boolean = false;
+  messageError:string = "";
+  constructor(private formBuilder: FormBuilder,private http: HttpClient){
+    this.authInsForm = this.formBuilder.group({
+      pseudo: ['' , Validators.required],
+      email: ['' , [Validators.required, Validators.email]],
+      mdp: ['' , Validators.required],
+      cmdp: ['' , Validators.required]
+    })
+  }
+  ngOnInit(): void {
+      
+  }
   onSubmit(){
+    this.formSubmitted = true;
+
     
+    if (!this.authInsForm.invalid) {
+      if (this.authInsForm.value.cmdp != this.authInsForm.value.mdp) {
+        this.messageError = "les mots de passe doivent être identique"
+      }else{
+        this.messageError = "";
+        
+        const formData = {
+          pseudo: this.authInsForm.value.pseudo,
+          email: this.authInsForm.value.email,
+          password: this.authInsForm.value.mdp
+        };
+
+        // this.http.put(environment.apiUrl+"/api/user/addUserA", formData).subscribe(
+        //   (response: any) => {
+        //     // console.log('Form submitted successfully', response);
+            
+            
+            
+        //   },
+        //   error => {
+        //     console.error('Error submitting form', error);
+        //   }
+        // );
+
+        console.log("CARRE")
+      }
+    }
   }
 }
