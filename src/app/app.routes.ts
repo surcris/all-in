0568586e14bae5811be14c26a3e9cdfc5b610750
Niel Abdallah <sympta113@gmail.com>
2024-06-01 09:57:41 +0200

    modif routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,7 +11,10 @@ import { ConnexionComponent } from './CultForm/connexion/connexion.component';
 
 export const routes: Routes = [
     { path: '', title: "accueil", component: HomeComponent },
-    { path: 'HomeCult', component: HomeCultFormComponent },
+    { path: 'HomeCult', loadComponent: () => import('./CultForm/home-cult-form/home-cult-form.component').then(m => m.HomeCultFormComponent) },
+    { path: 'Contact', loadComponent: () => import('./contact/contact.component').then(m => m.ContactComponent) },
+    { path: 'inscription',title:"inscription" , loadComponent: () => import('./CultForm/inscription/inscription.component').then(m => m.InscriptionComponent)},
+    { path: 'connexion',title:"connexion" , loadComponent: () => import('./CultForm/connexion/connexion.component').then(m => m.ConnexionComponent) },
 ];
 
 
