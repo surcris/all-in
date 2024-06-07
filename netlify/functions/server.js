a2397e507457e0b8bf2605321fc053083ad79dbe
Niel Abdallah <sympta113@gmail.com>
2024-06-07 03:36:28 +0200

    modif function server

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,24 +1,13 @@
-// server/server.js
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-
+const express = require('express');
 const serverless = require('serverless-http');
-dotenv.config();
 
 const app = express();
-app.use(cors());
+
 app.use(express.json());
 
-const port = process.env.PORT || 3000;
-
+// Définissez vos routes
 app.get('/api', (req, res) => {
-  res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify({ message: 'Hello from API' }));
+  res.json({ message: 'Hello from Serverless Function!' });
 });
-
-// app.listen(port, () => {
-//   console.log(`Server is running on port ${port}`);
-// });
 
 module.exports.handler = serverless(app);
