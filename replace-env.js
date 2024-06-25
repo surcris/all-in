9193a2cc06efe221b4fb5f979179e87d631bbad8
surcris <sympta113@gmail.com>
2024-06-25 19:05:30 +0200

    modif replace-env

--- replace-env.js
+++ replace-env.js
@@ -4,19 +4,35 @@ const path = require('path');
 const targetPath = './src/environments/environment.ts';
 const targetProdPath = './src/environments/environment.prod.ts';
 
+const firebaseConfig = `
+  firebaseConfig: {
+    apiKey: '${process.env.FIREBASE_API_KEY}',
+    authDomain: '${process.env.FIREBASE_AUTH_DOMAIN}',
+    databaseURL: '${process.env.FIREBASE_DATABASE_URL}',
+    projectId: '${process.env.FIREBASE_PROJECT_ID}',
+    storageBucket: '${process.env.FIREBASE_STORAGE_BUCKET}',
+    messagingSenderId: '${process.env.FIREBASE_MESSAGING_SENDER_ID}',
+    appId: '${process.env.FIREBASE_APP_ID}',
+    measurementId: '${process.env.FIREBASE_MEASUREMENT_ID}'
+  }
+`;
+
 const envProdConfigFile  = `export const environment = {
   production: ${process.env.NODE_ENV === 'production'},
   apiUrl: '${process.env.API_URL}',
-  akey: '${process.env.AKEY}'
+  akey: '${process.env.AKEY}',
+  ${firebaseConfig}
 };
 `;
 
 const envConfigFile = `export const environment = {
   production: ${process.env.NODE_ENV === 'production'},
   apiUrl: '${process.env.API_URL}',
-  akey: '${process.env.AKEY}'
+  akey: '${process.env.AKEY}',
+  ${firebaseConfig}
 };
 `;
+
 fs.writeFile(targetProdPath, envProdConfigFile, 'utf8', (err) => {
     if (err) {
       console.log('Error writing environment file', err);
